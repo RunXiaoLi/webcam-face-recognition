a27ec58163627bc53616bb754af1fe585b2e2328
RunXiaoLi <jiyishiweiyidexingli@gamil.com>
2023-04-28 13:34:28 +0800

    pref:切换sdapi

--- vue.config.js
+++ vue.config.js
@@ -7,10 +7,9 @@ module.exports = defineConfig({
         port:8888,
         proxy: {
             '/api': {
-                target: 'https://stablediffusionapi.com',
+                target: 'http://region-9.seetacloud.com:54421',
                 changeOrigin: true, // 是否跨域
                 ws: false, // 是否支持 websocket
-                secure: false, // 如果是 https 接口，需要配置这个参数
                 pathRewrite: { // 可以理解为一个重定向或者重新赋值的功能
                     '^/api': ''
                 }
